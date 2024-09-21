f4398a0e92c6900e24db46be689a9b084ea73393
trlk :P <petro200704@gmail.com>
2024-09-21 11:20:57 +0500

    first update

---
+++ python_intro/first_task.py
@@ -0,0 +1,24 @@
+#task №1
+print("Hello world!")
+
+#task №2
+one = float(input())
+two = float(input())
+print(one + two, one - two, one * two, one // two, one / two, one % two, one ** two)
+
+#task №3
+a = [2, 5, 2, 6] #and task №4
+len(a)
+print(len(a))
+
+#task №4
+b = int(input())
+print(a[b])
+
+#task №5
+dictionary = {'a1': 2, 'b2': 5} #and task №6
+i = input()
+print(dictionary[i])
+
+#task №6
+
